637648607e6dc03ee71f5ab779fd90401b3ca286
S.I.Shevchenko <sergey12491997@gmail.com>
2023-04-30 05:48:44 +0500

    revised dependencies and added descriptions for database update func

--- src/currencies/utils.py
+++ src/currencies/utils.py
@@ -1,3 +1,4 @@
+import asyncio
 import aiohttp
 import logging
 from datetime import datetime
@@ -9,7 +10,10 @@ from src.currencies.models import Currency
 from src.db import engine
 
 
-async def create_or_update_currency_info(api_key: str, code_name: list) -> dict:
+async def on_conflict_do_update_currency(api_key: str, code_name: list) -> dict:
+    """this func receives a currency code and its decryption as input and requests the API for the current
+    exchange rate of the requested currency, after which it writes the received data to the database """
+
     async with engine.begin() as session:
         code, name = code_name
         url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{code}"
@@ -27,7 +31,11 @@ async def create_or_update_currency_info(api_key: str, code_name: list) -> dict:
                     return conversion_rates
 
 
-async def start_create_or_update_currencies_info(api_key=API_KEY):
+async def start_create_or_update_currencies(api_key=API_KEY):
+    """this func receives an api_key (.env API_KEY by default) as input to access the exchangerate API,
+    receives a list of available codes from the API and calls the on_conflict_do_update_currency func,
+    passing it an api_key and a code from the list of available """
+
     logging.basicConfig(level=logging.INFO, filename="info_log.log", filemode="a")
     logging.info(f"DataBase update started at {datetime.utcnow()}")
     url = f"https://v6.exchangerate-api.com/v6/{api_key}/codes"
@@ -37,4 +45,13 @@ async def start_create_or_update_currencies_info(api_key=API_KEY):
             if response["result"] == "success":
                 supported_codes = response["supported_codes"]
                 for code_name in supported_codes:
-                    await create_or_update_currency_info(api_key=api_key, code_name=code_name)
+                    await on_conflict_do_update_currency(api_key=api_key, code_name=code_name)
+
+
+async def start_auto_update_db():
+    """this func starts an infinite loop of automatic update of the database once every 12 hours by calling
+    start_create_or_update_currencies and slowing down until the next call in the event_loop """
+
+    while True:
+        await start_create_or_update_currencies()
+        await asyncio.sleep(43200)  # 12 hours
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fastapi import FastAPI
 
 from src.currencies.router import router as currencies_router
-from src.currencies.utils import start_create_or_update_currencies_info
+from src.currencies.utils import start_auto_update_db
 
 app = FastAPI(
     title="Currency Converter"
@@ -14,14 +14,8 @@ app = FastAPI(
 app.include_router(currencies_router)
 
 
-async def start():
-    while True:
-        await start_create_or_update_currencies_info()
-        await asyncio.sleep(43200)  # 12 hours
-
-
 logging.basicConfig(level=logging.INFO, filename="info_log.log", filemode="a")
 logging.info(f"Project started at {datetime.utcnow()}\n{'-' * 100}")
 
 loop = asyncio.get_running_loop()
-loop.create_task(start())
+loop.create_task(start_auto_update_db())
