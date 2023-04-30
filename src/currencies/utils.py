71ffd60414823a3b61b5c74877174f0634df7cf4
S.I.Shevchenko <sergey12491997@gmail.com>
2023-04-30 06:04:38 +0500

    Small changes in the design of the file, the connection to the database is now opened before the start of writing data, and not at the start of the func

--- src/currencies/utils.py
+++ src/currencies/utils.py
@@ -1,7 +1,8 @@
-import asyncio
-import aiohttp
-import logging
 from datetime import datetime
+import asyncio
+import logging
+
+import aiohttp
 
 from sqlalchemy.dialects.postgresql import insert
 
@@ -10,28 +11,27 @@ from src.currencies.models import Currency
 from src.db import engine
 
 
-async def on_conflict_do_update_currency(api_key: str, code_name: list) -> dict:
+async def on_conflict_do_update_currency(api_key: str, code_name: list) -> None:
     """this func receives a currency code and its decryption as input and requests the API for the current
     exchange rate of the requested currency, after which it writes the received data to the database """
 
-    async with engine.begin() as session:
-        code, name = code_name
-        url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{code}"
-        async with aiohttp.ClientSession() as connection:
-            async with connection.get(url=url) as response:
-                response = await response.json()
-                if response["result"] == "success":
-                    conversion_rates = response["conversion_rates"]
+    code, name = code_name
+    url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{code}"
+    async with aiohttp.ClientSession() as connection:
+        async with connection.get(url=url) as response:
+            response = await response.json()
+            if response["result"] == "success":
+                conversion_rates = response["conversion_rates"]
+                async with engine.begin() as session:
                     stmt = insert(Currency).values(name=name, code=code, rates=conversion_rates)
                     on_conflict_do_update_stmt = stmt.on_conflict_do_update(
                         index_elements=["code", "id"],
                         set_=dict(name=name, rates=conversion_rates))
                     await session.execute(on_conflict_do_update_stmt)
                     await session.commit()
-                    return conversion_rates
 
 
-async def start_create_or_update_currencies(api_key=API_KEY):
+async def start_create_or_update_currencies(api_key=API_KEY) -> None:
     """this func receives an api_key (.env API_KEY by default) as input to access the exchangerate API,
     receives a list of available codes from the API and calls the on_conflict_do_update_currency func,
     passing it an api_key and a code from the list of available """
@@ -48,7 +48,7 @@ async def start_create_or_update_currencies(api_key=API_KEY):
                     await on_conflict_do_update_currency(api_key=api_key, code_name=code_name)
 
 
-async def start_auto_update_db():
+async def start_auto_update_db() -> None:
     """this func starts an infinite loop of automatic update of the database once every 12 hours by calling
     start_create_or_update_currencies and slowing down until the next call in the event_loop """
 
