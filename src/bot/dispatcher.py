f2d5efb2a1820fa51acb2ede249998fd3c8a28de
S.I.Shevchenko <sergey12491997@gmail.com>
2023-05-08 09:15:27 +0500

    Rename handler fenc

--- src/bot/dispatcher.py
+++ src/bot/dispatcher.py
@@ -3,22 +3,22 @@ from aiogram.filters import Command
 
 from src.config import BOT_TOKEN
 
-from src.bot.handler import get_start_bot
-from src.bot.handler import get_help_bot
-from src.bot.handler import get_all_supported_currency_bot
-from src.bot.handler import get_currency_rates_bot
-from src.bot.handler import get_conversion_bot
-from src.bot.handler import send_echo_bot
+from src.bot.handler import get_start_handler
+from src.bot.handler import get_help_handler
+from src.bot.handler import get_all_supported_currency_handler
+from src.bot.handler import get_currency_rates_handler
+from src.bot.handler import get_conversion_handler
+from src.bot.handler import send_echo_handler
 
 bot: Bot = Bot(token=BOT_TOKEN)
 dp: Dispatcher = Dispatcher()
 
-dp.message.register(get_start_bot, Command(commands=["start"]))
-dp.message.register(get_help_bot, Command(commands=['help']))
-dp.message.register(get_all_supported_currency_bot, Command(commands=['supported']))
-dp.message.register(get_currency_rates_bot, Command(commands=['rates']))
-dp.message.register(get_conversion_bot, Command(commands=['convert']))
-dp.message.register(send_echo_bot)
+dp.message.register(get_start_handler, Command(commands=["start"]))
+dp.message.register(get_help_handler, Command(commands=['help']))
+dp.message.register(get_all_supported_currency_handler, Command(commands=['supported']))
+dp.message.register(get_currency_rates_handler, Command(commands=['rates']))
+dp.message.register(get_conversion_handler, Command(commands=['convert']))
+dp.message.register(send_echo_handler)
 
 
 async def start_bot():
--- src/bot/handler.py
+++ src/bot/handler.py
@@ -9,24 +9,38 @@ from src.currencies.models import Currency
 from src.db import async_session_maker
 
 
-async def get_start_bot(message: Message):
+async def get_start_handler(message: Message):
     await message.answer("Привет! Я - бот конвертор валют!\n"
                          "Я умею:\n"
                          "  - Конвертировать одну валюту в другую\n"
                          "  - Показывать все поддерживаемые валюты для конвертации\n"
                          "  - Показывать все соотношения указанной валюты к поддерживаемым\n"
-                         "  - и еще немножко ;)")
+                         "  - и еще немножко ;)\n\n"
+                         "Инструкция:\n"
+                         "- Для конвертации одной валюты в другую\n"
+                         "    -- /covert код_валюты_1 код_валюты_2 колличество_валюты_1\n"
+                         "- Для полученя соотношения указанной валюты к поддерживаемым\n"
+                         "    -- /rates код_валюты\n"
+                         "- Для полученя списка поддерживаемых валют в формате \"код_валюты назание_валюты\"\n"
+                         "    -- /supported")
 
 
-async def get_help_bot(message: Message):
+async def get_help_handler(message: Message):
     await message.answer("Я умею:\n"
                          "  - Конвертировать одну валюту в другую\n"
                          "  - Показывать все поддерживаемые валюты для конвертации\n"
                          "  - Показывать все соотношения указанной валюты к поддерживаемым\n"
-                         "  - и еще немножко ;)\n")
+                         "  - и еще немножко ;)\n\n"
+                         "Инструкция:\n"
+                         "- Для конвертации одной валюты в другую\n"
+                         "    -- /covert код_валюты_1 код_валюты_2 колличество_валюты_1\n"
+                         "- Для полученя соотношения указанной валюты к поддерживаемым\n"
+                         "    -- /rates код_валюты\n"
+                         "- Для полученя списка поддерживаемых валют в формате \"код_валюты назание_валюты\"\n"
+                         "    -- /supported")
 
 
-async def get_conversion_bot(message: Message):
+async def get_conversion_handler(message: Message):
     try:
         base_code = message.text.split()[-3].upper()
         target_code = message.text.split()[-2].upper()
@@ -39,14 +53,14 @@ async def get_conversion_bot(message: Message):
                 rates = Decimal(currency.rates[target_code])
                 await message.answer(f"{quantity} {base_code} = {round(quantity * rates, 2)} {target_code}")
             except KeyError as ex:
-                await message.answer(f"{target_code}: Unsupported")
+                await message.answer(f"{target_code}: Unsupported\n/supported")
             except NoResultFound as ex:
-                await message.answer(f"{base_code}: Not found")
+                await message.answer(f"{base_code}: Not found\n/supported")
     except Exception as ex:
-        await message.answer(str(ex))
+        await message.answer("Ой, что-то пошло не так :(\n/help")
 
 
-async def get_currency_rates_bot(message: Message):
+async def get_currency_rates_handler(message: Message):
     try:
         target_code = message.text.split()[-1].upper()
         async with async_session_maker() as session:
@@ -58,12 +72,12 @@ async def get_currency_rates_bot(message: Message):
                                  "\n".join([f"{code}: {round(rate, 2)}" for code, rate in currency.rates.items()]))
             await message.answer(text)
         except NoResultFound as ex:
-            return {target_code: "Not found"}
+            return {target_code: "Not found\n/supported"}
     except Exception as ex:
-        await message.answer("Ой, что-то пошло не так :(")
+        await message.answer("Ой, что-то пошло не так :(\n/help")
 
 
-async def get_all_supported_currency_bot(message: Message):
+async def get_all_supported_currency_handler(message: Message):
     try:
         async with async_session_maker() as session:
             query = select(Currency.code, Currency.name)
@@ -74,9 +88,8 @@ async def get_all_supported_currency_bot(message: Message):
         await message.answer(str(ex))
 
 
-async def send_echo_bot(message: Message):
+async def send_echo_handler(message: Message):
     try:
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        await message.reply(text='Данный тип апдейтов не поддерживается '
-                                 'методом send_copy')
+        await message.reply(text="Ой, что-то пошло не так :(\n/help")
